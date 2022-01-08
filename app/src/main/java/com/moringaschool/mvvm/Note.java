29c4e71d48bc12b73700b7e002ad9cbfc87149fb
wdmuthiora <wdmuthiora@gmail.com>
2022-01-08 23:27:35 +0300

    Created Entity class. This is the same as Model, which represents an table in the SQLite table

---
+++ app/src/main/java/com/moringaschool/mvvm/Note.java
@@ -0,0 +1,45 @@
+package com.moringaschool.mvvm;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note-table") //Entity-This is a Room annotation, and at compile-time creates necessary code to create an SQLite table for this object.
+public class Note {
+    @PrimaryKey(autoGenerate = true) //PrimaryKey-Room annotation to create unique note ids in db.
+    private  int id;
+
+    private String title;
+    private String description;
+
+    @ColumnInfo(name = "priority_column") //ColumnInfo-Room annotation to define db column name of table. Default is the variable name, ie, priority.
+    private int priority;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+
+}
